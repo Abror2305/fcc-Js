541da1ec2287dbfde686133f9831560c143de8ff
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-01-05 19:51:59 +0500

    Use Bracket Notation to Find the First Character in a String

---
+++ use-bracket-notation-to-find-the-first-character-in-a-string.js
@@ -0,0 +1,10 @@
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+// Abror Alisherov
+// https://github.com/Abror2305/fcc-Js
+// https://t.me/AlisherovAbror
