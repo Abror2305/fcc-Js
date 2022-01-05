5a0e473a32e5546512b6fd1dc18d0bc68085a465
Abror Alisherov <74832377+Abror2305@users.noreply.github.com>
2022-01-05 19:46:38 +0500

    Find the Length of a String

---
+++ find-the-length-of-a-string.js
@@ -0,0 +1,10 @@
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// Abror Alisherov
+// https://github.com/Abror2305/fcc-Js
+// https://t.me/AlisherovAbror
